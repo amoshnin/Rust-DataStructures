de81f2a66f08037b237e0a9fbc9b0d2e2db25117
Artem <swift.uix@gmail.com>
2021-04-10 22:27:33 +0200

    macros tuple sym

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 #[macro_export]
 macro_rules! vector {
+    // Zero or more items with a delimiter of `,` between them //
     ($($item: expr), *) => {
         {
             let mut temp_vec = Vec::new();
-            $(
-              temp_vec.push($item);
-            )*
+            $(temp_vec.push($item);)*
             temp_vec
         }
     };
@@ -17,9 +16,27 @@ macro_rules! vector {
     }
 }
 
+#[macro_export]
+macro_rules! double_tuple_sum {
+    ( $($first_item: expr),*; $($second_item: expr),*) => {
+        {
+            let mut tuple = (0, 0);
+            $( tuple.0 += $first_item; )*
+            $( tuple.1 += $second_item; )*
+            tuple
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn double_tuple_sum_test() {
+        let result = double_tuple_sum!(1,2,3,4,5; 2,3,4,5,6);
+        assert_eq!(result, (15, 20));
+    }
 
     #[test]
     fn is_empty() {
@@ -35,6 +52,4 @@ mod tests {
     }
 }
 
-fn main() {
-
-}
+fn main() {}
